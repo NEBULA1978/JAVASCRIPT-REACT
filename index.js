6fcdc34a0c625a70edbdc4f6ce4c23caec78ce90
NEBULA1978 <rpascualey@gmail.com>
2023-07-24 16:27:58 +0200

    Template strings para colores boton

--- index.js
+++ index.js
@@ -1,23 +1,33 @@
-const background = 'red'
-const color = 'white'
+// VOY MINUTO 46
+const background = 'red';
+const color = 'white';
 
-const button = document.createElement('button')
-button.innerText = 'Click me'
-button.style = 'background: red; color :white'
+const result = `Esto son estilos ${background} ${color}`;
 
-const isAutorized = false
+console.log(result);
+
+const button = document.createElement('button');
+button.innerText = 'Click me';
+button.style='background:;color:white';
+
+
+const isAutorized = false;
 
 button.addEventListener('click', () => {
+  if (isAutorized) {
+    return alert('Estas autorizado');
+  } else {
+    return alert('No estas autorizado');
+  }
+});
 
-    // return 20
-    if (isAutorized) {
-        return alert('Estas autorizado')
-    } else {
-        return alert('No estas autorizado')
-    }
-})
+document.body.append(button);
 
-document.body.append(button)
+
+// Corrección de la función arrow que devuelve un objeto
+const showObject = () => ({ name: 'Ryan' });
+
+console.log(showObject());
 
 
 
