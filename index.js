8144761d3c2ed947f24f4e2ac5723761dfee9d31
NEBULA1978 <rpascualey@gmail.com>
2023-07-24 13:49:07 +0200

    Preparo para crear html con javascript creo h1 con un boton con el DOM

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const title = document.createElement('h1')
 title.innerText = 'Hola mundo desde JS'
 
+const button = document.createElement('button')
+button.innerText = "click";
+
 
 document.body.append(title)
+document.body.append(button);
