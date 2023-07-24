c3106ef00a6a1b85898d1c95355db1520348d45e
NEBULA1978 <rpascualey@gmail.com>
2023-07-24 18:45:22 +0200

    Con await forma moderna hacer peticion api con fetch

--- index3.js
+++ index3.js
@@ -1,17 +1,37 @@
+
+
+// fetch("https://jsonplaceholder.typicode.com/posts")
+//   .then(function (response) {
+//     return response.json();
+//   })
+//   .then(function (data) {
+//     console.log(data);
+//     data.forEach(function (post) {
+//       const li = document.createElement("li");
+//       li.innerText = post.title;
+//       ul.append(li); // Corrected line: append the li element to the ul
+//     });
+//     document.body.append(ul);
+//   });
+
+
+
+
+
 const ul = document.createElement("ul");
 
-fetch("https://jsonplaceholder.typicode.com/posts")
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-    data.forEach(function (post) {
-      const li = document.createElement("li");
-      li.innerText = post.title;
-      ul.append(li); // Corrected line: append the li element to the ul
-    });
-    document.body.append(ul);
+async function loaddata() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await response.json();
+  data.forEach(function (post) {
+    const li = document.createElement("li");
+    li.innerText = post.title;
+    ul.append(li);
   });
+}
+
+loaddata();
 
 console.log("Linea 2");
+
+document.body.appendChild(ul);
