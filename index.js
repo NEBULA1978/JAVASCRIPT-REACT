2a256e01b3f345f87bfb0d023aad15458e566243
NEBULA1978 <rpascualey@gmail.com>
2023-07-24 14:27:14 +0200

    Creando boton con el DOM cuando pulso alert mensage ,funcion anonima igual que commit anterior

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const button = document.createElement('button')
 button.innerText = 'Click me'
 
-function handlerClick(){
-    alert('click')
-}
 
 document.body.append(button)
-button.addEventListener("click", handlerClick);
+button.addEventListener('click', function(){
+    alert('click')
+})
