667a7d02e328fa9f2f6b346bd4d42da661ee4760
NEBULA1978 <rpascualey@gmail.com>
2023-07-24 13:16:41 +0200

    Ejemplo5 funcion con un parametro y un if y=0 y return suma un  numeros

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 // Ejemplo JS de componente en React
 
 function add(x, y) {
-  return x + y
+    if (y === undefined){
+        y = 0
+    }
+
+    return x + y
 }
 // Llamamos a una funcion y otra
-console.log(add(20, 20))//Hola Rayan
+console.log(add(20))//Hola Rayan
