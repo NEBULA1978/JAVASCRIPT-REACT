be957e6eab8fc58934c552b6177da6704f43e75e
NEBULA1978 <rpascualey@gmail.com>
2023-07-24 13:18:37 +0200

    Ejemplo5 funcion con dos parametros damos valores defecto x y y return suma un  numeros

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 // Ejemplo JS de componente en React
 
-function add(x, y) {
-    if (y === undefined){
-        y = 0
-    }
+function add(x = 23.3, y = 0) {
+
 
     return x + y
 }
 // Llamamos a una funcion y otra
-console.log(add(20))//Hola Rayan
+console.log(add())//Hola Rayan
