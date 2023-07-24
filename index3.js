85cfddb0fba43c1db5fad47c89b0e890691f276a
NEBULA1978 <rpascualey@gmail.com>
2023-07-24 18:34:28 +0200

    Promesas para extraer datos objeto json de api con fetch

--- index3.js
+++ index3.js
@@ -1,8 +1,17 @@
-fetch("https://jsonplaceholder.typicode.com/posts")
-.then(function (response) {
-  return response.json();
-}).then(function(data){
-    console.log(data)
-})
+const ul = document.createElement("ul");
 
-console.log('Linea 2')
+fetch("https://jsonplaceholder.typicode.com/posts")
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+    data.forEach(function (post) {
+      const li = document.createElement("li");
+      li.innerText = post.title;
+      ul.append(li); // Corrected line: append the li element to the ul
+    });
+    document.body.append(ul);
+  });
+
+console.log("Linea 2");
