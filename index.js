e1c7ea0878708026aa19ec28d3ec41fed3ce2ef8
NEBULA1978 <rpascualey@gmail.com>
2023-07-24 13:38:38 +0200

    Metodo lrgo de objeto para ver nombre  y precio

--- index.js
+++ index.js
@@ -1,55 +1,8 @@
-const user = {
-    // Propiedades property no tiene fuciones
-    name: 'Ryan',
-    lastname: 30,
-    age:20,
-    addres: {
-        country: 'Colombia',
-        city:'Bogota',
-        street: "Main street 123"
-    },
-    friends:['Brandom','Elena'],
-    active: true,
-    // Propiedades property no tiene fuciones hacia arriba
+const name = 'laptop'
+const price = 1000
 
-    // Metodos con funciones
-    sendMail() {
-        return 'sending email ...'
-    }
+const newProduct={
+    name: name,
+    price: price
 }
-
-console.log(user.addres.city);
-console.log(user.friends);
-console.log(user.active);
-console.log(user.sendMail());
-
-
-// Resultado
-// 20
-// friends
-// : 
-// Array(2)
-// 0
-// : 
-// "Brandom"
-// 1
-// : 
-// "Elena"
-// length
-// : 
-// 2
-// [[Prototype]]
-// : 
-// Array(0)
-// lastname
-// : 
-// 30
-// name
-// : 
-// "Ryan"
-// sendMail
-// : 
-// ƒ ()
-// [[Prototype]]
-// : 
-// Object
+console.log(newProduct)
