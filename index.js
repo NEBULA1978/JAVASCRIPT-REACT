54a556eb99d5aee7e9a7e118d531b1140f361a50
NEBULA1978 <rpascualey@gmail.com>
2023-07-24 14:32:35 +0200

    Creando boton con el DOM cuando pulso alert mensage ,ARROW FUNCTION igual que commit anterior

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
+// SON LO MISMO ARROWS FUNCTIONS
+// function add(x,y){
+    //     return x + y
+    // }
+    
+    // const add = (x,y) => {
+        //     return x +y
+        // }
+        
+        // SON LO MISMO ARROWS FUNCTIONS
+
+
 const button = document.createElement('button')
 button.innerText = 'Click me'
 
 
-document.body.append(button)
-button.addEventListener('click', function(){
+button.addEventListener('click', () => {
     alert('click')
 })
+
+document.body.append(button)
